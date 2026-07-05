ca5f43396404e681c7b2590c2b5bacf4db69acb3
Chathurani Nimesha <chathunimesha389@gmail.com>
2026-07-05 09:58:19 +0530

    GUI structure

--- iHungryBurgerShop.java
+++ iHungryBurgerShop.java
@@ -1,7 +1,18 @@
-import javax.swing.*;
 
-class iHungryBurgerShop{
+import javax.swing.JFrame;
+
+
+class iHungryBurgerShop extends JFrame{
+    public iHungryBurgerShop(){
+        setTitle("iHungryBurgerShop");
+        setSize(700,500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    //main method
     public static void main(String args[]){
-        
+        new iHungryBurgerShop();
     }
 }
